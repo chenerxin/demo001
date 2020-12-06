b0e44f9645b956300471fe728f3eaf7464496d60
chenerxin <714513240@qq.com>
2020-12-06 11:41:50 +0800

    mster制造冲突

--- src/main/java/com/example/demo001/Demo001Application.java
+++ src/main/java/com/example/demo001/Demo001Application.java
@@ -10,7 +10,9 @@ public class Demo001Application {
         //再模拟一下
         SpringApplication.run(Demo001Application.class, args);//模拟冲突11111
 
-        // 这里还有冲突呢
+        // 这里还有冲突呢 制造冲突
+
+        // 自己文件多了应该没问题吧
     }
 
 }
