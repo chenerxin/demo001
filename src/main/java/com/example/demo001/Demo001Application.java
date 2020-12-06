d20edff73abba0b499ce469cacdcc2db6791f6fe
chenerxin <714513240@qq.com>
2020-12-06 12:45:39 +0800

    制造冲突信息

--- src/main/java/com/example/demo001/Demo001Application.java
+++ src/main/java/com/example/demo001/Demo001Application.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo001Application {
-    private String abc111;
+    private String abc1112222;
     public static void main(String[] args) {
         //再模拟一下
         SpringApplication.run(Demo001Application.class, args);//模拟冲突11111神奇
 
-        // 1111222222222这里还有冲突呢 11111制造冲突111111
+        // 111188888888这里还有冲突呢 11111制造冲突111111
 
         // 自己文件多了应该没问题吧
     }
