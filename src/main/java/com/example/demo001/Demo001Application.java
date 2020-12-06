bb7d54cbc9f5119f52d23151a12a6a9a06cbb17e
chenerxin <714513240@qq.com>
2020-12-06 13:28:13 +0800

    合并提交信息

--- src/main/java/com/example/demo001/Demo001Application.java
+++ src/main/java/com/example/demo001/Demo001Application.java
@@ -5,14 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo001Application {
-    private String abc33333;
+    private String dev;
     public static void main(String[] args) {
         //再模拟一下
         SpringApplication.run(Demo001Application.class, args);//模拟冲突11111神奇
 
         // 1111222222222这里还有冲突呢 33333333
 
-        // 自己文件多了应该没问题吧
+        // 自己文件多了dev应该没问题吧
     }
 
 }
